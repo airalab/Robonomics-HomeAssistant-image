cf6a2de6ed5bf2a8f434a490c660d8a9adae136c
nakata5321 <makar5321@gmail.com>
2022-11-01 17:34:49 +0300

    first try

---
+++ robonomics-stage/00-home-assistant/01-run-chroot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+sudo useradd -rm homeassistant
+sudo mkdir /srv/homeassistant
+sudo chown homeassistant:homeassistant /srv/homeassistant
+
+cd /srv/homeassistant
+sudo -u homeassistant -H -s python3 -m venv .
+sudo -u homeassistant -H -s bash -c "source bin/activate &&
+        pip3 install wheel~=0.37"
+
+sudo -u homeassistant -H -s bash -c "source bin/activate &&
+				     pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
+
+sudo -u homeassistant -H -s bash -c "source bin/activate &&
+				     pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 &&
+        			     timeout 60s hass"
---
+++ robonomics-stage/02-zigbee-mqtt/00-run-chroot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
