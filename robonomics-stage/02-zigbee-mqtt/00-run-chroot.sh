2a2c64a184c41d299f6a71fa4b43a9a9c4a56458
nakata5321 <makar5321@gmail.com>
2022-11-02 18:02:09 +0300

    rebase develop

--- robonomics-stage/02-zigbee-mqtt/00-run-chroot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-apt-get install -y curl
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
--- robonomics-stage/02-zigbee-mqtt/02-run-chroot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-sudo mkdir /opt/zigbee2mqtt
-sudo chown -R ${USER}: /opt/zigbee2mqtt
-
-git clone --depth 1 --branch 1.28.0 https://github.com/Koenkk/zigbee2mqtt.git /opt/zigbee2mqtt
-
-cd /opt/zigbee2mqtt
-npm ci
