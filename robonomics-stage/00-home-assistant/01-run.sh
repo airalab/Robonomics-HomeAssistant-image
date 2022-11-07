9c4ac24aa0149969e31878092508a4b7a67dbb1d
nakata5321 <makar5321@gmail.com>
2022-11-07 14:49:51 +0300

    change user

--- robonomics-stage/00-home-assistant/01-run.sh
+++ robonomics-stage/00-home-assistant/01-run.sh
@@ -5,25 +5,24 @@ whoami
 on_chroot << EOF
     whoami
     pwd
-    HA=homeassistant
 
-    adduser --disabled-password --gecos "" ${HA}
+    adduser --disabled-password --gecos "" homeassistant
     install -d  /srv/homeassistant
-    chown ${HA}:${HA} /srv/homeassistant
+    chown homeassistant:homeassistant /srv/homeassistant
 
     cd /srv/homeassistant
 
     pwd
     whoami
 
-    su ${HA} -p -c python3 -m venv .
-    su ${HA} -p -c bash -c "source bin/activate &&
+    su homeassistant -p -c python3 -m venv .
+    su homeassistant -p -c bash -c "source bin/activate &&
             pip3 install wheel~=0.37"
 
-    su ${HA} -p -c bash -c "source bin/activate &&
+    su homeassistant -p -c bash -c "source bin/activate &&
                  pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
 
-    su ${HA} -p -c bash -c "source bin/activate &&
+    su homeassistant -p -c bash -c "source bin/activate &&
                  pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 &&
                        timeout 60s hass"
 EOF
