e392111a2ce05c1a6131e1219b98ec7909f697e3
nakata5321 <makar5321@gmail.com>
2022-11-07 14:19:43 +0300

    change commands

--- robonomics-stage/00-home-assistant/01-run.sh
+++ robonomics-stage/00-home-assistant/01-run.sh
@@ -4,20 +4,26 @@ whoami
 
 on_chroot << EOF
     whoami
+    pwd
+    HA=homeassistant
 
-    sudo useradd -rm homeassistant
-    sudo mkdir /srv/homeassistant
-    sudo chown homeassistant:homeassistant /srv/homeassistant
+    adduser --disabled-password --gecos "" ${HA}
+    install -d  /srv/homeassistant
+    chown ${HA}:${HA} /srv/homeassistant
 
     cd /srv/homeassistant
-    sudo -u homeassistant -H -s python3 -m venv .
-    sudo -u homeassistant -H -s bash -c "source bin/activate &&
+
+    pwd
+    whoami
+
+    su ${HA} -p -c python3 -m venv .
+    su ${HA} -p -c bash -c "source bin/activate &&
             pip3 install wheel~=0.37"
 
-    sudo -u homeassistant -H -s bash -c "source bin/activate &&
+    su ${HA} -p -c bash -c "source bin/activate &&
                  pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
 
-    sudo -u homeassistant -H -s bash -c "source bin/activate &&
+    su ${HA} -p -c bash -c "source bin/activate &&
                  pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 &&
                        timeout 60s hass"
 EOF
