cb789ffb5f7c091c54570caa6150b860ee2eb168
nakata5321 <makar5321@gmail.com>
2022-11-07 13:06:38 +0300

    use on_chroot

--- robonomics-stage/00-home-assistant/01-run.sh
+++ robonomics-stage/00-home-assistant/01-run.sh
@@ -1,19 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
 whoami
 
-useradd -rm homeassistant
-mkdir /srv/homeassistant
-chown homeassistant:homeassistant /srv/homeassistant
+on_chroot << EOF
+    whoami
 
-cd /srv/homeassistant
-sudo -u homeassistant -H -s python3 -m venv .
-sudo -u homeassistant -H -s bash -c "source bin/activate &&
-        pip3 install wheel~=0.37"
+    sudo useradd -rm homeassistant
+    sudo mkdir /srv/homeassistant
+    sudo chown homeassistant:homeassistant /srv/homeassistant
 
-sudo -u homeassistant -H -s bash -c "source bin/activate &&
-				     pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
+    cd /srv/homeassistant
+    sudo -u homeassistant -H -s python3 -m venv .
+    sudo -u homeassistant -H -s bash -c "source bin/activate &&
+            pip3 install wheel~=0.37"
 
-sudo -u homeassistant -H -s bash -c "source bin/activate &&
-				     pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 &&
-        			     timeout 60s hass"
+    sudo -u homeassistant -H -s bash -c "source bin/activate &&
+                 pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
+
+    sudo -u homeassistant -H -s bash -c "source bin/activate &&
+                 pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 &&
+                       timeout 60s hass"
+EOF
