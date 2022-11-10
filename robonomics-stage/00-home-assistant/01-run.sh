39ac0a747bf4cb35675538b8c38dfc2d2e475a50
nakata5321 <makar5321@gmail.com>
2022-11-10 16:37:21 +0300

    one line commands

--- robonomics-stage/00-home-assistant/01-run.sh
+++ robonomics-stage/00-home-assistant/01-run.sh
@@ -16,13 +16,9 @@ on_chroot << EOF
     whoami
 
     su homeassistant -p -c python3 -m venv .
-    su homeassistant -p -c bash -c "source bin/activate &&
-            pip3 install wheel~=0.37"
+    su homeassistant -p -c bash -c "source bin/activate && pip3 install wheel~=0.37"
 
-    su homeassistant -p -c bash -c "source bin/activate &&
-                 pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
+    su homeassistant -p -c bash -c "source bin/activate && pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
 
-    su homeassistant -p -c bash -c "source bin/activate &&
-                 pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 &&
-                       timeout 60s hass"
+    su homeassistant -p -c bash -c "source bin/activate && pip3 install homeassistant==2022.10.3 psutil-home-assistant~=0.0 && timeout 60s hass"
 EOF
