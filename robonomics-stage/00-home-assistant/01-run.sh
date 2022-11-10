76375994401d955b2f37851c9fa66722e523121a
nakata5321 <makar5321@gmail.com>
2022-11-10 18:21:05 +0200

    fix python problem

--- robonomics-stage/00-home-assistant/01-run.sh
+++ robonomics-stage/00-home-assistant/01-run.sh
@@ -15,10 +15,11 @@ on_chroot << EOF
     pwd
     whoami
 
-    su homeassistant -p -c python3 -m venv .
+    su homeassistant -p -c "python3 -m venv ."
+
     ls
 
-    su homeassistant -p -c bash source bin/activate && pip3 install wheel~=0.37
+    su homeassistant -p -c bash -c "source bin/activate && pip3 install wheel~=0.37"
 
     su homeassistant -p -c bash -c "source bin/activate && pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
 
