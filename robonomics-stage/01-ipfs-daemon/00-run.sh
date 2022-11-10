de6c3f69026208a478d370012fc16021f54371ba
nakata5321 <makar5321@gmail.com>
2022-11-10 19:02:50 +0200

    fix hass

--- robonomics-stage/01-ipfs-daemon/00-run.sh
+++ robonomics-stage/01-ipfs-daemon/00-run.sh
@@ -1,36 +1,39 @@
 #!/bin/bash -e
 
-cd /home/${FIRST_USER_NAME}
-wget https://dist.ipfs.io/go-ipfs/v0.14.0/go-ipfs_v0.14.0_linux-arm64.tar.gz
-tar -xvzf go-ipfs_v0.14.0_linux-arm64.tar.gz
-rm go-ipfs_v0.14.0_linux-arm64.tar.gz
-cd go-ipfs
-bash install.sh
-ipfs init -p local-discovery
-ipfs bootstrap add /dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8
-ipfs bootstrap add /dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9
-ipfs bootstrap add /dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw
+on_chroot << EOF
+
+  cd /home/${FIRST_USER_NAME}
+  su ${FIRST_USER_NAME} -p -c "wget https://dist.ipfs.io/go-ipfs/v0.14.0/go-ipfs_v0.14.0_linux-arm64.tar.gz"
+  su ${FIRST_USER_NAME} -p -c "tar -xvzf go-ipfs_v0.14.0_linux-arm64.tar.gz"
+  su ${FIRST_USER_NAME} -p -c "rm go-ipfs_v0.14.0_linux-arm64.tar.gz"
+  cd go-ipfs
+  su ${FIRST_USER_NAME} -p -c "bash install.sh"
+  su ${FIRST_USER_NAME} -p -c "ipfs init -p local-discovery"
+  su ${FIRST_USER_NAME} -p -c "ipfs bootstrap add /dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8"
+  su ${FIRST_USER_NAME} -p -c "ipfs bootstrap add /dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9"
+  su ${FIRST_USER_NAME} -p -c "ipfs bootstrap add /dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw"
 
 
-cd /home/${FIRST_USER_NAME}
-curl -O https://raw.githubusercontent.com/LoSk-p/robonomics-hass-utils/main/raspberry_pi/ipfs_check.sh
-chmod a+x ipfs_check.sh
-mv ipfs_check.sh /usr/local/bin/
+  cd /home/${FIRST_USER_NAME}
+  su ${FIRST_USER_NAME} -p -c "curl -O https://raw.githubusercontent.com/LoSk-p/robonomics-hass-utils/main/raspberry_pi/ipfs_check.sh"
+  chmod a+x ipfs_check.sh
+  mv ipfs_check.sh /usr/local/bin/
 
 
-echo "[Unit]
-Description=IPFS Daemon Service
+  echo "[Unit]
+  Description=IPFS Daemon Service
 
-[Service]
-Type=simple
-ExecStartPre=/usr/local/bin/ipfs_check.sh
-ExecStart=/usr/local/bin/ipfs daemon
-User=ubuntu
+  [Service]
+  Type=simple
+  ExecStartPre=/usr/local/bin/ipfs_check.sh
+  ExecStart=/usr/local/bin/ipfs daemon
+  User=ubuntu
 
-[Install]
-WantedBy=multi-user.target
+  [Install]
+  WantedBy=multi-user.target
 
-" | tee /etc/systemd/system/ipfs-daemon.service
+  " | tee /etc/systemd/system/ipfs-daemon.service
 
-sudo systemctl enable ipfs-daemon.service
-sudo systemctl start ipfs-daemon.service
+  systemctl enable ipfs-daemon.service
+  systemctl start ipfs-daemon.service
+EOF
