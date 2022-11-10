5850faa4c0de5d351e52c29a21734f9c02396f91
nakata5321 <makar5321@gmail.com>
2022-11-10 17:56:27 +0200

    delete command key

--- robonomics-stage/00-home-assistant/01-run.sh
+++ robonomics-stage/00-home-assistant/01-run.sh
@@ -16,7 +16,9 @@ on_chroot << EOF
     whoami
 
     su homeassistant -p -c python3 -m venv .
-    su homeassistant -p -c bash -c "source bin/activate && pip3 install wheel~=0.37"
+    ls
+
+    su homeassistant -p -c bash source bin/activate && pip3 install wheel~=0.37
 
     su homeassistant -p -c bash -c "source bin/activate && pip3 install sqlalchemy~=1.4 fnvhash~=0.1 aiodiscover==1.4.11"
 
