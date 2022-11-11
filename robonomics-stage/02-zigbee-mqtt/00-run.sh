8bcf0fdade42f0d96a5886003e7d283b39e257dc
nakata5321 <makar5321@gmail.com>
2022-11-11 13:47:41 +0200

    update root commands

--- robonomics-stage/02-zigbee-mqtt/00-run.sh
+++ robonomics-stage/02-zigbee-mqtt/00-run.sh
@@ -2,6 +2,6 @@
 
 on_chroot << EOF
 
-  su ${FIRST_USER_NAME} -c "curl -fsSL https://deb.nodesource.com/setup_16.x | bash -"
+  curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 
 EOF
--- robonomics-stage/03-robonomics-integration/00-run.sh
+++ robonomics-stage/03-robonomics-integration/00-run.sh
@@ -2,6 +2,22 @@
 
 on_chroot << EOF
 
+  echo "[Unit]
+  Description=Home Assistant
+  After=network-online.target
+  [Service]
+  Type=simple
+  Restart=on-failure
+  User=%i
+  WorkingDirectory=/srv/%i/
+  ExecStart=/srv/homeassistant/bin/hass -c "/home/%i/.homeassistant"
+  Environment="PATH=/srv/%i/bin"
+  [Install]
+  WantedBy=multi-user.target
+  " | tee /etc/systemd/system/home-assistant@homeassistant.service
+
+  systemctl enable home-assistant@homeassistant.service
+
   cd /srv/homeassistant
 
   su homeassistant -c "source bin/activate && pip3 install robonomics-interface~=1.3"
