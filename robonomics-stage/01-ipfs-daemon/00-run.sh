3ead4fc03c802ad43edceb25d497906b0cd79dd2
nakata5321 <makar5321@gmail.com>
2022-12-13 01:18:02 +0200

    update ipfs user

--- robonomics-stage/01-ipfs-daemon/00-run.sh
+++ robonomics-stage/01-ipfs-daemon/00-run.sh
@@ -27,7 +27,7 @@ on_chroot << EOF
   Type=simple
   ExecStartPre=/usr/local/bin/ipfs_check.sh
   ExecStart=/usr/local/bin/ipfs daemon
-  User=ubuntu
+  User=smart
 
   [Install]
   WantedBy=multi-user.target
