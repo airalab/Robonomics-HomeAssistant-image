ada6356745a224a2dc8c58977f8eccc2605514c7
nakata5321 <makar5321@gmail.com>
2023-01-23 12:07:57 +0700

    update yggdrasil installation

---
+++ robonomics-stage/01-yggdrasil/01-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+    cd
+    wget https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.7/yggdrasil-0.4.7-arm64.deb
+    dpkg -i yggdrasil-0.4.7-arm64.deb
+
+EOF
