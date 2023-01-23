17499fbc31443a5939c085a8b97876a404a33d14
nakata5321 <makar5321@gmail.com>
2023-01-23 16:29:58 +0700

    stop yggdrasil and delete config file

--- robonomics-stage/01-yggdrasil/01-run.sh
+++ robonomics-stage/01-yggdrasil/01-run.sh
@@ -4,5 +4,7 @@ on_chroot << EOF
     cd
     wget https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.7/yggdrasil-0.4.7-arm64.deb
     dpkg -i yggdrasil-0.4.7-arm64.deb
+    systemctl stop yggdrasil
+    rm /etc/yggdrasil.conf
 
 EOF
