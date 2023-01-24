bb1bacf2f1f88b9c884d9cbdfc58e3128adb8ea3
nakata5321 <makar5321@gmail.com>
2023-01-24 16:23:06 +0700

    download peers file

--- robonomics-stage/01-yggdrasil/01-run.sh
+++ robonomics-stage/01-yggdrasil/01-run.sh
@@ -6,4 +6,7 @@ on_chroot << EOF
     dpkg -i yggdrasil-0.4.7-arm64.deb
     rm /etc/yggdrasil.conf
 
+    cd /home/${FIRST_USER_NAME}
+    curl -O https://raw.githubusercontent.com/airalab/robonomics-hass-utils/main/raspberry_pi/input.json
+
 EOF
