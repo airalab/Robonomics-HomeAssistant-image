65e466165d87162b081077d834703395ccb90050
Makar Chernyaev <55352168+nakata5321@users.noreply.github.com>
2023-08-02 11:37:15 +0300

    Bookworm (#4)
    * test creating bookworm debian
    
    * update release variable
    
    * switch to my fork
    
    * update python package version
    
    * add npm package
    
    * update packages
    
    * update robonomics integration
    
    * Test refactor (#3)
    
    * change homeassistant home dir
    
    * update scripts
    
    * fix paths
    
    * fix path for HA
    
    * update username
    
    * dicrease HA version
    
    * return to airalab directory
    
    * update release yml file

--- robonomics-stage/02-ipfs-daemon/00-run.sh
+++ robonomics-stage/02-ipfs-daemon/00-run.sh
@@ -2,12 +2,12 @@
 
 on_chroot << EOF
 
-  adduser --disabled-password --gecos "" ipfsdaemon
+  useradd -rm ipfsdaemon -d /var/lib/ipfsdaemon
 
-  cd /home/ipfsdaemon
-  su ipfsdaemon -c "wget https://dist.ipfs.io/go-ipfs/v0.17.0/go-ipfs_v0.17.0_linux-arm64.tar.gz"
-  su ipfsdaemon -c "tar -xvzf go-ipfs_v0.17.0_linux-arm64.tar.gz"
-  su ipfsdaemon -c "rm go-ipfs_v0.17.0_linux-arm64.tar.gz"
+  cd /var/lib/ipfsdaemon/
+  su ipfsdaemon -c "wget https://dist.ipfs.io/go-ipfs/v0.21.0/go-ipfs_v0.21.0_linux-arm64.tar.gz"
+  su ipfsdaemon -c "tar -xvzf go-ipfs_v0.21.0_linux-arm64.tar.gz"
+  su ipfsdaemon -c "rm go-ipfs_v0.21.0_linux-arm64.tar.gz"
   cd go-ipfs
   bash install.sh
   su ipfsdaemon -c "ipfs init -p local-discovery"
@@ -16,7 +16,7 @@ on_chroot << EOF
   su ipfsdaemon -c "ipfs bootstrap add /dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw"
 
 
-  cd /home/ipfsdaemon
+  cd /var/lib/ipfsdaemon/
   su ipfsdaemon -c "curl -O https://raw.githubusercontent.com/airalab/robonomics-hass-utils/main/raspberry_pi/ipfs_first_start.sh"
   chmod a+x ipfs_first_start.sh
   mv ipfs_first_start.sh /usr/local/bin/
@@ -28,9 +28,9 @@ After=network.target
 
 [Service]
 Type=simple
+User=ipfsdaemon
 ExecStartPre=/usr/local/bin/ipfs_first_start.sh
 ExecStart=/usr/local/bin/ipfs daemon --enable-gc
-User=ipfsdaemon
 
 [Install]
 WantedBy=multi-user.target
